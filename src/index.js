1f6fe7334f22d42ac353eb92adf1299c9bf96757
Abhimanyu Hans <abhimanyu.hans30@gmail.com>
2021-03-08 22:02:29 +0530

    changed react version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './common/Header';
+import Login from './screens/login/Login';
 
 ReactDOM.render(
     <Header /> , document.getElementById('root')
